f793bc5c60490480a1af41769f123bf0ce8a00e9
David Koenig <david.koenig@msg.group>
2020-01-09 14:40:10 +0100

    fixed issue when target server not available

--- setup/start.sh
+++ setup/start.sh
@@ -2,30 +2,31 @@
 
 INITIALSTART=0
 
-echo "running start.sh script of nginx"
+echo "start.sh: Running start.sh script of nginx"
 
 # set initstart variable
 if [ ! -f /home/appuser/data/nginx/firststart.flg ]
 then 
-    echo "first start, set initialstart variable to 1"
+    echo "start.sh: First start, set initialstart variable to 1"
     INITIALSTART=1
     echo `date +%Y-%m-%d_%H:%M:%S_%z` > /home/appuser/data/nginx/firststart.flg
 else
-	echo "It's not the first start, skip first start section"
+	echo "start.sh: It's not the first start, skip first start section"
 fi
 
 if [ "$INITIALSTART" == "1" ]
 then
-	echo "First start. Create initial certificates"
+	echo "start.sh: First start. Create initial certificates"
 	/home/appuser/app/helper/createcerts.sh
+	if [ "$?" == "1" ]; then exit 1; fi
 else
-	echo "It's not the first start, skip first start section"
+	echo "start.sh: It's not the first start, skip first start section"
 fi
 
-echo "test nginx configuration file"
+echo "start.sh: Test nginx configuration file"
 nginx -t -c /home/appuser/data/nginx/nginx.cnf
 
-echo "start nginx"
+echo "start.sh: Start nginx"
 nginx -c /home/appuser/data/nginx/nginx.cnf
 
 #touch /tmp/debug.log
